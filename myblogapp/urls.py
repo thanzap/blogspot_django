20974ef871fb5ed55c144af0d9879ce664034f78
thanzap <thanzap@gmail.com>
2021-12-29 12:54:46 +0200

    first-commit

---
+++ myblogapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path('',views.index,name="index"), #empty quotes because its the root url (main page)
+    path('post/<str:pk>',views.blogpost, name='post')
+]
